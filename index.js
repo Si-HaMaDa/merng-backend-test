2a83221e1c742d932c9492b671bdab2b062bbb1a
Si HaMaDa <nrookman_27@yahoo.com>
2021-02-13 02:24:24 +0200

    edit to env vars

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = process.env.MONGODB || require('./config.js');
+const { MONGODB } = require('./config.js') || process.env.MONGODB;
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 5001;
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
